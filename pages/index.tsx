b5a9bdfb672d162410d65ac489280a83eba9cbd9
adamwojnicki <wojnicki.adam1@gmail.com>
2022-04-19 12:21:21 +0200

    initial setup

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div className='text-green-600'>
+      {/* HEADER */}
+      {/* CAROUSEL */}
+      {/* GAME LIST */}
+    </div>
+  )
+}
+
+export default index
