3fc68900e2f8ddbb1a1270f846609800ae555d1c
adamwojnicki <wojnicki.adam1@gmail.com>
2022-05-05 09:48:49 +0200

    splide js install + warning fix

--- components/GamePlatforms/GamePlatforms.tsx
+++ components/GamePlatforms/GamePlatforms.tsx
@@ -6,7 +6,13 @@ const GamePlatforms = ({ platforms }: { platforms: string[] }) => {
     Windows: <Windows className="mx-1 w-6 h-6 text-darkBlue" />,
     Linux: <Linux className="mx-1 w-6 h-6 text-darkBlue" />,
   };
-  return <div className="flex">{platforms.map((p) => ic[p])}</div>;
+  return (
+    <div className="flex">
+      {platforms.map((p) => (
+        <div key={p}>{ic[p]}</div>
+      ))}
+    </div>
+  );
 };
 
 export default GamePlatforms;
