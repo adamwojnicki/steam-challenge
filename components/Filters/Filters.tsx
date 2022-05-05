164238712bbe4f7b9b850050a8f7ab418ee1e0e1
adamwojnicki <wojnicki.adam1@gmail.com>
2022-05-05 11:49:12 +0200

    h2 added

--- components/Filters/Filters.tsx
+++ components/Filters/Filters.tsx
@@ -2,28 +2,31 @@ import React from "react";
 
 const Filters = () => {
   return (
-    <div className="mb-6 sm:flex justify-between">
-      <input
-        className="bg-darkBlue2 px-6 py-2 rounded-full mb-3 sm:mb-0"
-        type="text"
-        name="search"
-        placeholder="Search"
-        id=""
-      />
-      <div>
-        <label className="mr-2" htmlFor="sort">
-          Sort by:
-        </label>
-        <select
-          className="bg-darkBlue2 px-6 py-2 rounded-full"
-          name="sort"
-          id="sort"
-        >
-          <option value="price">Price</option>
-          <option value="name">Name</option>
-        </select>
+    <>
+      <h2 className="text-2xl mb-4">New & Trending</h2>
+      <div className="mb-6 sm:flex justify-between">
+        <input
+          className="bg-darkBlue2 px-6 py-2 rounded-full mb-3 sm:mb-0"
+          type="text"
+          name="search"
+          placeholder="Search"
+          id=""
+        />
+        <div>
+          <label className="mr-2" htmlFor="sort">
+            Sort by:
+          </label>
+          <select
+            className="bg-darkBlue2 px-6 py-2 rounded-full"
+            name="sort"
+            id="sort"
+          >
+            <option value="price">Price</option>
+            <option value="name">Name</option>
+          </select>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
