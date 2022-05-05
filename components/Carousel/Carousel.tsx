42a251f6de1f261fce1e2e156205a6954d4f5fdb
adamwojnicki <wojnicki.adam1@gmail.com>
2022-05-05 10:19:36 +0200

    basic slider functionality

--- components/Carousel/Carousel.tsx
+++ components/Carousel/Carousel.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
-const Carousel = () => {
-  return <section className="container py-6">[Carousel placeholder]</section>;
+interface Game {
+  id: string;
+  image: string;
+  link: string;
+  title: string;
+}
+
+const Carousel = ({ games }) => {
+  return (
+    <section className="container py-6">
+      <Splide
+        className="max-w-4xl mx-auto"
+        options={{
+          perPage: 1,
+          rewind: true,
+          gap: "1rem",
+        }}
+      >
+        {games.map((g: Game) => (
+          <SplideSlide key={g.id}>
+            <a href={g.link}>
+              <img className="w-full rounded-xl" src={g.image} alt={g.title} />
+            </a>
+          </SplideSlide>
+        ))}
+      </Splide>
+    </section>
+  );
 };
 
 export default Carousel;
